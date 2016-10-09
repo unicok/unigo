166ff9bf4bf8d6a786f940d4976f554d5629a382
amorwilliams <amorwilliams@hotmail.com>
2016-10-09 22:29:58 +0800

    add chat.proto

--- tool/pb-gen.sh
+++ tool/pb-gen.sh
@@ -2,7 +2,9 @@
 
 ROOT_PATH=$PWD/..
 
-cd ROOT_PATH/lib/proto/game
-protoc  ./*.proto --go_out=plugins=grpc:.
-cd ROOT_PATH/lib/proto/snowflake
-protoc  ./*.proto --go_out=plugins=grpc:.
+cd $ROOT_PATH/lib/proto/game
+protoc  ./*.proto --go_out=plugins=grpc:./
+cd $ROOT_PATH/lib/proto/snowflake
+protoc  ./*.proto --go_out=plugins=grpc:./
+cd $ROOT_PATH/lib/proto/Chat
+protoc  ./*.proto --go_out=plugins=grpc:./
